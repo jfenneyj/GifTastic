b3597ec3373002cf295805a462c7e17efe2eb4ed
jfenneyj <jfenney@leadtomarket.com>
2018-06-03 10:17:04 -0700

    update files1

---
+++ gif.js
@@ -0,0 +1,7 @@
+// display giphy & bottons on load
+//input for text for add animal with submit button
+//submit button should add 10 more gifs 
+// when clicking on image the gif plays when clicked again it stop playing.
+// q = 
+// limit = 10 gifs
+//rating
