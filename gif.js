a23464c78dea038049d8c84c1abef35d785f28b8
jfenneyj <jfenney@leadtomarket.com>
2018-06-05 10:29:06 -0700

    updated file3

--- gif.js
+++ gif.js
@@ -11,30 +11,63 @@ $(document).ready(function () {
 
     // array of animals
     var q = "animals";
-
+    var rating = "&pg-13";
+    // initial array of animals
     var animals = ["bird", "cat", "lizard", "dog", "dolphin", "seal", "monkey"];
 
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q + "&api_key=bnVtRNFiLdPLfyMM7a9hfp9nys3Npfpj&limit=10";
-    
-    console.log(queryURL);
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + q +
+        "&api_key=bnVtRNFiLdPLfyMM7a9hfp9nys3Npfpj&limit=10";
+
+    // console.log(queryURL);
+
+    /*function alertAnimalName(){
+        var animalName = $(this).attr("data-name");
+        NOT WORKING!!
+        console.log(animalName);
+    }*/
 
     //create buttons
-    function printBtn() {
-      
+    function renderButtons() {
+
+        $("#animal-view").empty();
 
         for (var i = 0; i < animals.length; i++) {
 
-           $(animals).each(function(){});
+            //create tag element
+            var btn = $("<button>");
+            //adding a class to buttons
+            btn.addClass("animal");
+            //adding a data-attribute
+            btn.attr("data-name", animals[i]);
+            //initial button text
+            btn.text(animals[i]);
+            // adding the buttons to the HTML
+            $("#animal-view").append(btn);
 
-            var btn = document.createElement("button");
-            var animal = document.createTextNode(animals[i]);
-            btn.appendChild(animal);
-            document.body.appendChild(btn);
-            
+        }
     }
-   
-    }
-    printBtn();
+         //calling the function for the buttons
+         renderButtons();
+
+    // event handler function on click;
+    $("#add-animal").on("click", function (evt) {
+        //prevents the form from its default behavior
+        evt.preventDefault();
+
+        //grabs the text from the input box
+        var animal = $("#animal-input").val().trim();
+
+        //animals from input box will be added to our array
+        animals.push(animal);
+        // alert(animal);
+
+         //calling the function for the buttons
+    renderButtons();
+
+    });
+
+
+
 
 
 });
